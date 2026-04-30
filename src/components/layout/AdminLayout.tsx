5e0097b92821fb97aac5cbb17fc0df78b64588ee
PSpabitra <sarkarpabi42@gmail.com>
2026-04-30 16:33:46 +0530

    admin layout header footer added

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -23,11 +23,16 @@ const AdminLayout = ({ children }: AdminLayoutProps) => {
       <div 
         className={`flex-1 flex flex-col min-w-0 transition-all duration-500 ${isSidebarOpen ? 'ml-64' : 'ml-0 md:ml-[88px]'}`}
       >
+                <Header variant="landing" />
+        
         <main className="flex-1 p-6">
           <div className="max-w-7xl mx-auto w-full mt-4">
             {children}
           </div>
         </main>
+                
+        <Footer />
+
       </div>
     </div>
   );
